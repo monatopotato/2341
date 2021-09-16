3510d1b2f825504583bc44cdead09ac57914e7f3
monatopotato <65310139+monatopotato@users.noreply.github.com>
2021-09-15 20:17:55 -0400

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,10 @@
+class atm:
+    cardNum = input("what is your card number? ")
+    pinNum = input("what is your pin number")
+
+def CashWithdrawl():
+    ask = int(input("How much will you withdraw?"))
+    print("You withdrawed",ask,"dollars from your bank account")
+
+def BalanceEnquiry():
+    print("your balance is ",0-ask)
